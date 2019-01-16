73b59e9e52d3770a278d1866b564041f9e47a41c
KIPR-Submissions <46693830+KIPR-Submissions@users.noreply.github.com>
2019-01-16 12:51:52 -0600

    Adding main.c file
    adding initial file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <kipr/botball.h>
+
+//This program was created on the Wallaby. It will be used to test uploading
+//to GitHub and making changes via multiple users.
+
+int main()
+{
+    printf("Hello, my name is Ashley \n");
+    //Do nothing right now
+   
+    return 0;
+}
